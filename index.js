b5456d832c8a148dd75ec46294d173ce095ce964
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-09-22 16:36:44 +0300

    step 4

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ const express = require("express")
 const app = express()
 const date = new  Date()
 
+
 app.get('/',(req,res)=>{
     res.send("OK")
 })
 
 app.get('/test',(req,res)=>{
-    res.send(`{
-        status:200,
-        message:"ok"
-    }`)
+    res.send(`{ status:200, message:"ok"}`)
 })
 
 app.get('/time',(req,res)=>{
-    res.send(`{
-        status:200,
-        message: ${date.getHours()}:${date.getMinutes()}
-        
-    }`)
+    res.send(`{status:200, message: ${date.getHours()}:${date.getMinutes()}}`)
+})
+
+app.get('/hello/:id?',(req,res)=>{
+    const id = req.params.id || 'word'
+    res.send(`{status:200, message:"Hello,"${id}}`)
 })
 
 

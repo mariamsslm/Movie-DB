e6cde5418d26809199940dad72943cf558c1d350
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-09-23 14:37:24 +0300

    step 5

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express")
 const app = express()
 const date = new  Date()
-
+const movies = [
+     { 
+        title: 'Jaws',
+        year: 1975,
+        rating: 8 
+     }, 
+     {
+        title: 'Avatar',
+        year: 2009,
+        rating: 7.8 
+     }, 
+     {
+        title: 'Brazil',
+        year: 1985,
+        rating: 8 
+    },
+    { 
+        title: 'الإرهاب والكباب',
+        year: 1992,
+        rating: 6.2 
+    }]
+    
 
 app.get('/',(req,res)=>{
     res.send("OK")
@@ -19,16 +40,29 @@ app.get('/hello/:id?',(req,res)=>{
     const id = req.params.id || 'word'
     res.send(`{status:200, message:"Hello,"${id}}`)
 })
-app.get('/search/:s?',(req,res)=>{
+app.get('/search',(req,res)=>{
+
     const s = req.query.s 
     if(s) {
-        res.send(`{status:200, message:"Hello,"${s}}`)
+        res.send({status:200, message:s})
 
     }
     else{
-        res.send(` {status:500,error:true, message:"you have to provide a search"}`)
+        res.send(`{status:500,error:true, message:"you have to provide a search"}`)
 
-    }
+    }    
+})
+app.get('/movies/add',(req,res)=>{
+    
+})
+app.get('/movies/get',(req,res)=>{
+    res.send({status:200, data:movies })
+    
+})
+app.get('/movies/edit',(req,res)=>{
+    
+})
+app.get('/movies/delete',(req,res)=>{
     
 })
 
